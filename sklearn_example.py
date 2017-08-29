5beb0d3e992d54a4a2b22d1044109c0a18006119
Philip Cho <chohyu01@cs.washington.edu>
2017-08-28 22:30:35 -0700

    Address idiosyncracies of scikit-learn model

--- sklearn_example.py
+++ sklearn_example.py
@@ -6,15 +6,17 @@ import sys
 
 # load a multi-class classification problem
 X, y = load_wine(return_X_y=True)
-ntree = 30
+ntree = 5
 
 # 1. random forest
 clf = RandomForestClassifier(random_state=1, n_estimators=ntree)
 clf.fit(X, y)
 
 builder = ModelBuilder(num_feature=clf.n_features_,
-                       num_output_group=clf.n_classes_,
-                       params={'pred_transform':'max_index'})
+                       num_output_group=clf.n_classes_)
+                       params={'pred_transform':'identity'})
+                       # no need for softmax here, as each leaf node already
+                       # produces a probability distribution
 
 for i in range(ntree):
   clf_tree = clf.estimators_[i].tree_
@@ -25,6 +27,8 @@ for i in range(ntree):
   tree[0].set_root()
   for nid in range(nnode):
     if clf_tree.children_left[nid] == -1:  # leaf
+      # scikit-learn random forests remember the distribution of classes
+      # among the training instances in each leaf node.
       tree[nid].set_leaf_node(node_value[nid] / node_value[nid].sum())
     else:  # non-leaf
       tree[nid].set_numerical_test_node(
@@ -40,7 +44,12 @@ compiler = Compiler()
 compiler.generate_code(model, 'test_rf', params={}, verbose=True)
 
 # 2. gradient boosting
-clf = GradientBoostingClassifier(random_state=1, n_estimators=ntree, max_depth=4)
+clf = GradientBoostingClassifier(random_state=1, n_estimators=ntree,
+                                 max_depth=4, init='zero')
+                           # set init='zero' to start boosting from zero.
+                           # scikit-learn by default starts boosting from
+                           # the prior distribution of classes, but tree-lite
+                           # does not yet support boosting from nonzero.
 clf.fit(X, y)
 
 builder = ModelBuilder(num_feature=clf.n_features_,
@@ -48,7 +57,7 @@ builder = ModelBuilder(num_feature=clf.n_features_,
                        params={'pred_transform':'softmax'})
 
 for i in range(ntree):
-  for k in range(clf.n_classes_):    
+  for k in range(clf.n_classes_):
     clf_tree = clf.estimators_[i][k].tree_
     nnode = clf_tree.node_count
     node_value = clf_tree.value.squeeze()
@@ -57,7 +66,9 @@ for i in range(ntree):
     tree[0].set_root()
     for nid in range(nnode):
       if clf_tree.children_left[nid] == -1:  # leaf
-        tree[nid].set_leaf_node(node_value[nid])
+        tree[nid].set_leaf_node(clf.learning_rate * node_value[nid])
+            # for scikit-learn gradient booster, each leaf value is "unscaled";
+            # we'd need to shrink it by the learning rate.
       else:  # non-leaf
         tree[nid].set_numerical_test_node(
                                     feature_id=clf_tree.feature[nid],

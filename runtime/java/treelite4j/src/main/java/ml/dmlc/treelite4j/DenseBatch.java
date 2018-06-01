618d8df06f683eeb3b8dc7862cac91b14d6a46f3
Philip Cho <chohyu01@cs.washington.edu>
2018-05-31 18:04:47 -0700

    Implement JNI / Java wrapper functions

---
+++ runtime/java/treelite4j/src/main/java/ml/dmlc/treelite4j/DenseBatch.java
@@ -0,0 +1,48 @@
+package ml.dmlc.treelite4j;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+
+/**
+ * 2D dense batch, laid out in row-major layout
+ * @author Philip Cho
+ */
+class DenseBatch {
+  private float[] data;
+  private float missing_value;
+  private int num_row;
+  private int num_col;
+
+  private long handle;
+
+  public DenseBatch(
+    float[] data, float missing_value, int num_row, int num_col)
+      throws TreeliteError {
+    this.data = data;
+    this.missing_value = missing_value;
+    this.num_row = num_row;
+    this.num_col = num_col;
+
+    long[] out = new long[1];
+    TreeliteJNI.checkCall(TreeliteJNI.TreeliteAssembleDenseBatch(
+      this.data, this.missing_value, this.num_row, this.num_col, out));
+    handle = out[0];
+  }
+
+  public long getHandle() {
+    return this.handle;
+  }
+
+  @Override
+  protected void finalize() throws Throwable {
+    super.finalize();
+    dispose();
+  }
+
+  public synchronized void dispose() {
+    if (handle != 0L) {
+      TreeliteJNI.TreeliteDeleteDenseBatch(handle);
+      handle = 0;
+    }
+  }
+}
---
+++ runtime/java/treelite4j/src/main/java/ml/dmlc/treelite4j/SparseBatch.java
@@ -0,0 +1,50 @@
+package ml.dmlc.treelite4j;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+
+/**
+ * 2D sparse batch, laid out in CSR (Compressed Sparse Row) layout
+ * @author Philip Cho
+ */
+class SparseBatch {
+  private float[] data;
+  private int[] col_ind;
+  private long[] row_ptr;
+  private int num_row;
+  private int num_col;
+
+  private long handle;
+
+  public SparseBatch(
+    float[] data, int[] col_ind, long[] row_ptr, int num_row, int num_col)
+      throws TreeliteError {
+    this.data = data;
+    this.col_ind = col_ind;
+    this.row_ptr = row_ptr;
+    this.num_row = num_row;
+    this.num_col = num_col;
+
+    long[] out = new long[1];
+    TreeliteJNI.checkCall(TreeliteJNI.TreeliteAssembleSparseBatch(
+      this.data, this.col_ind, this.row_ptr, this.num_row, this.num_col, out));
+    handle = out[0];
+  }
+
+  public long getHandle() {
+    return this.handle;
+  }
+
+  @Override
+  protected void finalize() throws Throwable {
+    super.finalize();
+    dispose();
+  }
+
+  public synchronized void dispose() {
+    if (handle != 0L) {
+      TreeliteJNI.TreeliteDeleteSparseBatch(handle);
+      handle = 0;
+    }
+  }
+}

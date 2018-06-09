6aeca123e6f3d9d8fb3369048510b78d5dcafe56
James Liu <jameschl@amazon.com>
2018-06-09 13:18:01 -0700

    Enable set/get QValue methods in Data interface

--- runtime/java/ml/dmlc/treelite/Data.java
+++ runtime/java/ml/dmlc/treelite/Data.java
@@ -3,7 +3,9 @@ package ml.dmlc.treelite;
 
 public interface Data {
   public void setFValue(float val);
+  public void setQValue(int val);
   public void setMissing();
   public boolean isMissing();
   public float getFValue();
+  public int getQValue();
 }

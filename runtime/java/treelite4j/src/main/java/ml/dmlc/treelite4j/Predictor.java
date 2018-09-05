1da63266396840bd1d60ca468c3f705d6dd92771
Philip Cho <chohyu01@cs.washington.edu>
2018-09-05 11:31:37 -0700

    Add a clarify note in Java predict method

--- runtime/java/treelite4j/src/main/java/ml/dmlc/treelite4j/Predictor.java
+++ runtime/java/treelite4j/src/main/java/ml/dmlc/treelite4j/Predictor.java
@@ -173,8 +173,10 @@ public class Predictor {
 
   /**
    * Perform batch prediction with a 2D dense data matrix. Worker threads
-   * will internally divide up work for batch prediction. Note that this
-   * function may be called by only one thread at a time.
+   * will internally divide up work for batch prediction. **Note that this
+   * function may be called by only one thread at a time.** In order to use
+   * multiple threads to process multiple prediction requests simultaneously,
+   * use :java:meth:`Predictor.predict(Data[], boolean)` instead.
    * @param batch a :java:ref:`DenseBatch`, representing a slice of a 2D dense
    *              matrix
    * @param verbose whether to print extra diagnostic messages

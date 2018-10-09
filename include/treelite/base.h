79eb179f1bd21b59fa625b7d49e5ce5a8390db86
Philip Hyunsu Cho <chohyu01@cs.washington.edu>
2018-10-08 19:39:50 -0700

    Use double-precision to store split thresholds (#55)

--- include/treelite/base.h
+++ include/treelite/base.h
@@ -14,7 +14,7 @@
 namespace treelite {
 
 /*! \brief float type to be used internally */
-typedef float tl_float;
+typedef double tl_float;
 /*! \brief feature split type */
 enum class SplitFeatureType : int8_t {
   kNone, kNumerical, kCategorical

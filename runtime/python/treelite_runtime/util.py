d2c578ce350c3f39bcff8510a575849f8108a581
Hyunsu Cho <chohyu01@cs.washington.edu>
2020-06-10 15:16:01 -0700

    Revert "Use long path name on Windows"
    This reverts commit 03580ccdd61e2a0c6c057cf45bd0c4a2ff244958.

--- runtime/python/treelite_runtime/util.py
+++ runtime/python/treelite_runtime/util.py
@@ -5,7 +5,6 @@ Miscellaneous utilities
 import inspect
 import ctypes
 import time
-import sys
 from sys import platform as _platform
 
 
@@ -45,25 +44,3 @@ def lib_extension_current_platform():
     if _platform in ('win32', 'cygwin'):
         return '.dll'
     return '.so'
-
-
-def expand_windows_path(dirpath):
-    """
-    Expand a short path to full path (only applicable for Windows)
-    Parameters
-    ----------
-    dirpath : :py:class:`str <python:str>`
-        Path to expand
-    Returns
-    -------
-    fullpath : :py:class:`str <python:str>`
-        Expanded path
-    """
-    if sys.platform == 'win32':
-        BUFFER_SIZE = 500
-        buffer = ctypes.create_unicode_buffer(BUFFER_SIZE)
-        get_long_path_name = ctypes.windll.kernel32.GetLongPathNameW
-        get_long_path_name.argtypes = [ctypes.c_wchar_p, ctypes.c_wchar_p, ctypes.c_uint]
-        get_long_path_name(dirpath, buffer, BUFFER_SIZE)
-        return buffer.value
-    return dirpath

46512551defa92a04b6db9b60962c495d2b69933
Hyunsu Cho <chohyu01@cs.washington.edu>
2020-06-10 15:57:24 -0700

    Add generate_cmakelists() to API doc

--- python/treelite/__init__.py
+++ python/treelite/__init__.py
@@ -7,11 +7,11 @@ import os
 from .core import DMatrix
 from .frontend import Model, ModelBuilder
 from .annotator import Annotator
-from .contrib import create_shared, generate_makefile
+from .contrib import create_shared, generate_makefile, generate_cmakelists
 
 VERSION_FILE = os.path.join(os.path.dirname(__file__), 'VERSION')
 with open(VERSION_FILE, 'r') as _f:
     __version__ = _f.read().strip()
 
 __all__ = ['DMatrix', 'Model', 'ModelBuilder', 'Annotator', 'create_shared', 'generate_makefile',
-           'gallery']
+           'generate_cmakelists', 'gallery']

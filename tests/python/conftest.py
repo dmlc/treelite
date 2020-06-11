20f976399f3555502b61353d1b96f3a5150ef19e
Hyunsu Cho <chohyu01@cs.washington.edu>
2020-06-11 11:43:27 -0700

    Try fixing tempdir on Windows one last time

--- tests/python/conftest.py
+++ tests/python/conftest.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 """Pytest fixtures to initialize tests"""
+import os
 import tempfile
 
 import pytest
@@ -8,20 +9,20 @@ from .metadata import dataset_db
 
 
 @pytest.fixture(scope='session')
-def annotation(tmp_path_factory):
+def annotation():
     """Pre-computed branch annotation information for example datasets"""
-    tmpdir = tmp_path_factory.mktemp('annotation')
-    def compute_annotation(dataset):
-        model = treelite.Model.load(dataset_db[dataset].model,
-                                    model_format=dataset_db[dataset].format)
-        if dataset_db[dataset].dtrain is None:
-            return None
-        dtrain = treelite.DMatrix(dataset_db[dataset].dtrain)
-        annotator = treelite.Annotator()
-        annotator.annotate_branch(model=model, dmat=dtrain, verbose=True)
-        annotation_path = str(tmpdir / f'{dataset}.json')
-        annotator.save(annotation_path)
-        with open(annotation_path, 'r') as f:
-            return f.read()
-    annotation_db = {k: compute_annotation(k) for k in dataset_db}
+    with tempfile.TemporaryDirectory(dir='.') as tmpdir:
+        def compute_annotation(dataset):
+            model = treelite.Model.load(dataset_db[dataset].model,
+                                        model_format=dataset_db[dataset].format)
+            if dataset_db[dataset].dtrain is None:
+                return None
+            dtrain = treelite.DMatrix(dataset_db[dataset].dtrain)
+            annotator = treelite.Annotator()
+            annotator.annotate_branch(model=model, dmat=dtrain, verbose=True)
+            annotation_path = os.path.join(tmpdir, f'{dataset}.json')
+            annotator.save(annotation_path)
+            with open(annotation_path, 'r') as f:
+                return f.read()
+        annotation_db = {k: compute_annotation(k) for k in dataset_db}
     return annotation_db

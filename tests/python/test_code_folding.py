facbbc8f0f4ad24a0576a619fdfe6aaf0d99ac73
Hyunsu Cho <chohyu01@cs.washington.edu>
2020-06-11 00:19:02 -0700

    Make test_code_folding.py faster

--- tests/python/test_code_folding.py
+++ tests/python/test_code_folding.py
@@ -12,22 +12,21 @@ from .util import os_compatible_toolchains, check_predictor, does_not_raise
 
 @pytest.mark.parametrize('code_folding_factor', [0.0, 1.0, 2.0, 3.0])
 @pytest.mark.parametrize('toolchain', os_compatible_toolchains())
-@pytest.mark.parametrize('use_annotation', [True, False])
 @pytest.mark.parametrize('dataset', ['dermatology', 'letor', 'toy_categorical'])
-def test_code_folding(tmpdir, annotation, dataset, use_annotation, toolchain, code_folding_factor):
+def test_code_folding(tmpdir, annotation, dataset, toolchain, code_folding_factor):
     """Test suite for testing code folding feature"""
     libpath = os.path.join(tmpdir, dataset_db[dataset].libname + _libext())
     model = treelite.Model.load(dataset_db[dataset].model, model_format=dataset_db[dataset].format)
     annotation_path = os.path.join(tmpdir, 'annotation.json')
 
-    if use_annotation:
-        if annotation[dataset] is None:
-            pytest.skip('No training data available. Skipping annotation')
+    if annotation[dataset] is None:
+        annotation_path = None
+    else:
         with open(annotation_path, 'wb') as f:
             f.write(annotation[dataset])
 
     params = {
-        'annotate_in': (annotation_path if use_annotation else 'NULL'),
+        'annotate_in': (annotation_path if annotation_path else 'NULL'),
         'quantize': 1,
         'parallel_comp': model.num_tree,
         'code_folding_req': code_folding_factor

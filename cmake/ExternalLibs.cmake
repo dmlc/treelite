cc428362f271bf69e89a5da657bdd3b29a544a42
Hyunsu Cho <chohyu01@cs.washington.edu>
2020-09-08 16:24:40 -0700

    Add RapidJSON as a dependency

--- cmake/ExternalLibs.cmake
+++ cmake/ExternalLibs.cmake
@@ -15,6 +15,24 @@ FetchContent_Declare(
 FetchContent_MakeAvailable(fmtlib)
 set_target_properties(fmt PROPERTIES EXCLUDE_FROM_ALL TRUE)
 
+# RapidJSON (header-only library)
+add_library(rapidjson INTERFACE)
+find_package(RapidJSON)
+if(RapidJSON_FOUND)
+  target_include_directories(rapidjson INTERFACE ${RAPIDJSON_INCLUDE_DIRS})
+else()
+  message(STATUS "Did not found RapidJSON in the system root. Fetching RapidJSON now...")
+  FetchContent_Declare(
+    RapidJSON
+    GIT_REPOSITORY      https://github.com/Tencent/rapidjson
+    GIT_TAG             v1.1.0
+  )
+  FetchContent_Populate(RapidJSON)
+  message(STATUS "RapidJSON was downloaded at ${rapidjson_SOURCE_DIR}.")
+  target_include_directories(rapidjson INTERFACE $<BUILD_INTERFACE:${rapidjson_SOURCE_DIR}/include>)
+endif()
+add_library(RapidJSON::rapidjson ALIAS rapidjson)
+
 # Google C++ tests
 if(BUILD_CPP_TEST)
   find_package(GTest)
---
+++ tests/cpp/test_frontend.cc
@@ -0,0 +1,20 @@
+/*!
+ * Copyright (c) 2020 by Contributors
+ * \file test_frontend.cc
+ * \author Hyunsu Cho
+ * \brief C++ tests for frontends
+ */
+#include <gtest/gtest.h>
+#include <treelite/tree.h>
+#include <treelite/frontend.h>
+
+namespace treelite {
+
+TEST(XGBoostJSONFrontend, Basic) {
+  std::string json_str = "{}";
+
+  Model model;
+  frontend::LoadXGBoostJSONModel(json_str, &model);
+}
+
+}  // namespace treelite

32efb6539ce11a5daae8fe64f0fd92c3baab89f0
Hyunsu Cho <chohyu01@cs.washington.edu>
2020-11-30 23:32:50 +0000

    Add a test with a toy example

--- tests/python/test_lightgbm_integration.py
+++ tests/python/test_lightgbm_integration.py
@@ -3,6 +3,7 @@
 import os
 
 import numpy as np
+import scipy.sparse
 import pytest
 import treelite
 import treelite_runtime
@@ -149,6 +150,60 @@ def test_categorical_data(tmpdir, quantize, parallel_comp, toolchain):
     model.export_lib(toolchain=toolchain, libpath=libpath, params=params, verbose=True)
     predictor = treelite_runtime.Predictor(libpath=libpath, verbose=True)
     check_predictor(predictor, dataset)
+
+
+@pytest.mark.parametrize('toolchain', os_compatible_toolchains())
+@pytest.mark.parametrize('quantize', [True, False])
+def test_sparse_ranking_model(tmpdir, quantize, toolchain):
+    """Generate a LightGBM ranking model with highly sparse data.
+    This example is inspired by https://github.com/dmlc/treelite/issues/222. It verifies that
+    Treelite is able to accommodate the unique behavior of LightGBM when it comes to handling
+    missing values.
+
+    LightGBM offers two modes of handling missing values:
+
+    1. Assign default direction per each test node: This is similar to how XGBoost handles missing
+       values.
+    2. Replace missing values with zeroes (0.0): This behavior is unique to LightGBM.
+
+    The mode is controlled by the missing_value_to_zero_ field of each test node.
+
+    This example is crafted so as to invoke the second mode of missing value handling.
+    """
+    rng = np.random.default_rng(seed=2020)
+    X = scipy.sparse.random(m=10, n=206947, format='csr', dtype=np.float64, random_state=0,
+                            density=0.0001)
+    X.data = rng.standard_normal(size=X.data.shape[0], dtype=np.float64)
+    y = rng.integers(low=0, high=5, size=X.shape[0])
+
+    params = {
+        'objective': 'lambdarank',
+        'num_leaves': 32,
+        'lambda_l1': 0.0,
+        'lambda_l2': 0.0,
+        'min_gain_to_split': 0.0,
+        'learning_rate': 1.0,
+        'min_data_in_leaf': 1
+    }
+
+    model_path = os.path.join(tmpdir, 'sparse_ranking_lightgbm.txt')
+    libpath = os.path.join(tmpdir, f'sparse_ranking_lgb' + _libext())
+
+    dtrain = lightgbm.Dataset(X, label=y, group=[X.shape[0]])
+
+    bst = lightgbm.train(params, dtrain, num_boost_round=1)
+    lgb_out = bst.predict(X)
+    bst.save_model(model_path)
+
+    model = treelite.Model.load(model_path, model_format='lightgbm')
+    model.export_lib(toolchain=toolchain, libpath=libpath, verbose=True)
+
+    predictor = treelite_runtime.Predictor(libpath, verbose=True)
+
+    dmat = treelite_runtime.DMatrix(X)
+    out = predictor.predict(dmat)
+
+    np.testing.assert_almost_equal(out, lgb_out)
 
 
 @pytest.mark.parametrize('toolchain', os_compatible_toolchains())

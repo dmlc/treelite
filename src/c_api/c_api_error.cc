19671e9e9b2e6cd00c5e790539f7197775634a2e
Hyunsu Cho <chohyu01@cs.washington.edu>
2021-07-07 19:30:55 -0700

    Remove use of dmlc::thread_local

--- src/c_api/c_api_error.cc
+++ src/c_api/c_api_error.cc
@@ -1,17 +1,18 @@
 /*!
- * Copyright (c) 2017-2020 by Contributors
+ * Copyright (c) 2017-2021 by Contributors
  * \file c_api_error.cc
  * \author Hyunsu Cho
  * \brief C error handling
  */
-#include <dmlc/thread_local.h>
+#include <treelite/thread_local.h>
 #include <treelite/c_api_error.h>
+#include <string>
 
 struct TreeliteAPIErrorEntry {
   std::string last_error;
 };
 
-typedef dmlc::ThreadLocalStore<TreeliteAPIErrorEntry> TreeliteAPIErrorStore;
+using TreeliteAPIErrorStore = treelite::ThreadLocalStore<TreeliteAPIErrorEntry>;
 
 const char* TreeliteGetLastError() {
   return TreeliteAPIErrorStore::Get()->last_error.c_str();

035269a15942d03e4c5f837c2b26f538fc11a2b2
Philip Hyunsu Cho <chohyu01@cs.washington.edu>
2021-07-10 02:44:55 -0700

    Update ast_native.cc

--- src/compiler/ast_native.cc
+++ src/compiler/ast_native.cc
@@ -7,7 +7,6 @@
 #include <treelite/compiler.h>
 #include <treelite/compiler_param.h>
 #include <treelite/annotator.h>
-#include <dmlc/io.h>
 #include <fmt/format.h>
 #include <rapidjson/stringbuffer.h>
 #include <rapidjson/writer.h>

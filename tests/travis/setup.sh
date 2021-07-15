5a5577c28b4a2c3cf5f92c8a5920e0770ad2c41a
Hyunsu Cho <chohyu01@cs.washington.edu>
2021-07-14 18:25:40 -0700

    [CI] Downgrade libomp to 11.1.0

--- tests/travis/setup.sh
+++ tests/travis/setup.sh
@@ -3,6 +3,12 @@
 set -eo pipefail
 
 if [ ${TRAVIS_OS_NAME} == "osx" ]; then
+  brew update
+  # Use libomp 11.1.0: https://github.com/dmlc/xgboost/issues/7039
+  wget https://raw.githubusercontent.com/Homebrew/homebrew-core/679923b4eb48a8dc7ecc1f05d06063cd79b3fc00/Formula/libomp.rb -O $(find $(brew --repository) -name libomp.rb)
+  brew install cmake libomp lcov ninja
+  brew pin libomp
+
   wget -O conda.sh https://repo.anaconda.com/miniconda/Miniconda3-latest-MacOSX-x86_64.sh
 else
   wget -O conda.sh https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh

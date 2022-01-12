e41cf7301727492b88a529ec15cb42629d410fc8
Hyunsu Cho <chohyu01@cs.washington.edu>
2022-01-12 22:02:56 +0000

    Fix lint

--- tests/python/test_invalid_categocial_input.py
+++ tests/python/test_invalid_categocial_input.py
@@ -1,3 +1,6 @@
+# -*- coding: utf-8 -*-
+# pylint: disable=missing-function-docstring
+"""Test whether Treelite handles invalid category values correctly"""
 import os
 
 import treelite
@@ -8,8 +11,8 @@ from treelite.contrib import _libext
 
 from .util import os_compatible_toolchains
 
-@pytest.fixture
-def toy_model():
+@pytest.fixture(name='toy_model')
+def toy_model_fixture():
     builder = treelite.ModelBuilder(num_feature=1)
     tree = treelite.ModelBuilder.Tree()
     tree[0].set_categorical_test_node(feature_id=0, left_categories=[0], default_left=True,
@@ -22,12 +25,12 @@ def toy_model():
 
     return model
 
-@pytest.fixture
-def test_data():
+@pytest.fixture(name='test_data')
+def test_data_fixture():
     return np.array([[-1], [-0.6], [-0.5], [0], [0.3], [0.7], [1]], dtype=np.float32)
 
-@pytest.fixture
-def ref_pred():
+@pytest.fixture(name='ref_pred')
+def ref_pred_fixture():
     # Negative inputs are mapped to the right child node
     # 0.3 and 0.7 are mapped to the left child node, since they get rounded toward the zero.
     return np.array([1, 1, 1, -1, -1, -1, 1], dtype=np.float32)

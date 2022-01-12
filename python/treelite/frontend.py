76d8e79fa8082be867e7cc44c6c3096bec8d3309
Philip Hyunsu Cho <chohyu01@cs.washington.edu>
2022-01-11 19:36:35 -0800

    Fix from_xgboost() for models with categorical splits (#333)
    * Fix from_xgboost() for models with categorical splits
    
    * Fix gtest
    
    * Fix lint
    
    * Provision for num_target field
    
    * Fix lint
    
    * Check XGBoost version

--- python/treelite/frontend.py
+++ python/treelite/frontend.py
@@ -7,6 +7,7 @@ import shutil
 import os
 import json
 from tempfile import TemporaryDirectory
+from pkg_resources import parse_version
 
 import numpy as np
 
@@ -370,7 +371,6 @@ class Model:
            bst = xgboost.train(params, dtrain, 10, [(dtrain, 'train')])
            tl_model = Model.from_xgboost(bst)
         """
-        handle = ctypes.c_void_p()
         # attempt to import xgboost
         try:
             import xgboost
@@ -379,6 +379,13 @@ class Model:
                                 '`xgboost.Booster` object') from e
         if not isinstance(booster, xgboost.Booster):
             raise ValueError('booster must be of type `xgboost.Booster`')
+        if parse_version(xgboost.__version__) >= parse_version('1.0.0'):
+            # TODO(hcho3): Remove this line once XGBoost implements a method to save JSON model
+            # in-memory. (Right now, save_model() always saves to the disk.)
+            model_json_str = booster.__getstate__()['handle'].decode('utf-8')
+            return cls.from_xgboost_json(model_json_str)
+        # If pre-1.0.0 version of XGBoost is used, use legacy serialization
+        handle = ctypes.c_void_p()
         buffer = booster.save_raw()
         ptr = (ctypes.c_char * len(buffer)).from_buffer(buffer)
         length = ctypes.c_size_t(len(buffer))

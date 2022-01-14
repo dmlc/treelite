e2162123133e38df7ed7e860ebff3badd2e8d668
Philip Hyunsu Cho <chohyu01@cs.washington.edu>
2022-01-14 12:57:55 -0800

    Fix PyBuffer serializer (#340)

---
+++ tests/cython/test_serialization.py
@@ -0,0 +1,17 @@
+"""Test for serialization, via buffer protocol"""
+import treelite
+from sklearn.datasets import load_iris
+from sklearn.ensemble import RandomForestClassifier
+from serializer import treelite_serialize, treelite_deserialize
+
+def test_serialize_as_buffer():
+    """Test whether Treelite objects can be serialized to a buffer"""
+    X, y = load_iris(return_X_y=True)
+    clf = RandomForestClassifier(max_depth=3, random_state=0, n_estimators=10)
+    clf.fit(X, y)
+
+    tl_model = treelite.sklearn.import_model(clf)
+    frames = treelite_serialize(tl_model)
+    # This call should succeed
+    tl_model2 = treelite_deserialize(frames)
+    # This call should succeed too

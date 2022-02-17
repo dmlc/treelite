a4ba980f018a19c8d2a61733ac899e9237911b8e
Hyunsu Cho <chohyu01@cs.washington.edu>
2022-02-17 11:53:15 -0800

    Use correct wheel

--- tests/travis/run_test.sh
+++ tests/travis/run_test.sh
@@ -112,9 +112,8 @@ if [ ${TASK} == "python_sdist_test" ]; then
   python -m pip install -v treelite_runtime-*.tar.gz
 
   # Run tests
-  # Install XGBoost and LightGBM without OpenMP
-  python -m pip install https://s3-us-west-2.amazonaws.com/xgboost-nightly-builds/master/xgboost-1.6.0.dev0%2B12949c6b3134817f2f38ca765f42d9fdbda6e600-py3-none-macosx_10_15_x86_64.macosx_11_0_x86_64.macosx_12_0_x86_64.whl
-  python -m pip install lightgbm --no-binary :all:
+  python -m pip install https://s3-us-west-2.amazonaws.com/xgboost-nightly-builds/master/xgboost-1.6.0.dev0%2B12949c6b3134817f2f38ca765f42d9fdbda6e600-py3-none-manylinux2014_x86_64.whl
+  python -m pip install lightgbm
   python -m pytest -v --fulltrace tests/python
 
   # Deploy source wheel to S3

71bd8eafa4627f389b50638d99037f63c02b2ea3
Hyunsu Philip Cho <chohyu01@cs.washington.edu>
2022-08-31 16:45:51 -0700

    Fix env activation

--- ops/build-windows-part2.bat
+++ ops/build-windows-part2.bat
@@ -1,14 +1,14 @@
-echo "##[section]Running C++ tests..."
+echo ##[section]Running C++ tests...
 .\build\treelite_cpp_test.exe
 
-echo "##[section]Setting up Python environment..."
+echo ##[section]Setting up Python environment...
 conda create -n dev -y -q -c conda-forge python=3.9 numpy scipy scikit-learn pandas
 call activate dev
 
-echo "##[section]Packaging Python wheel for Treelite..."
+echo ##[section]Packaging Python wheel for Treelite...
 cd python
 python setup.py bdist_wheel --universal
 
-echo "##[section]Packaging Python wheel for Treelite runtime..."
+echo ##[section]Packaging Python wheel for Treelite runtime...
 cd ..\runtime\python
 python setup.py bdist_wheel --universal

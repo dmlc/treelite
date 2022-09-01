c12605e17a0e58604a3669223162323386b9c5db
Hyunsu Philip Cho <chohyu01@cs.washington.edu>
2022-08-31 21:34:02 -0700

    Fix

--- ops/build-macos.sh
+++ ops/build-macos.sh
@@ -6,7 +6,7 @@ echo "##[section]Building MacOS Python wheels..."
 tests/ci_build/build_python_wheels.sh ${CIBW_PLATFORM_ID} ${COMMIT_ID}
 
 echo "##[section]Uploading MacOS Python wheels to S3..."
-conda activate python3
+source activate python3
 python --version
 python -m pip install awscli
 python -m awscli s3 cp treelite-*.whl s3://treelite-wheels/ --acl public-read || true

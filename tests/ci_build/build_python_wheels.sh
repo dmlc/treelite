6d60cc986ee2ab13ddfc29d3455b7bae3555088b
Hyunsu Philip Cho <chohyu01@cs.washington.edu>
2022-08-31 21:29:23 -0700

    Fix conda path

--- tests/ci_build/build_python_wheels.sh
+++ tests/ci_build/build_python_wheels.sh
@@ -51,7 +51,7 @@ if [[ "$platform_id" == macosx_* ]]; then
 
     sudo conda create -n build $OPENMP_URL
     conda info -e
-    PREFIX="$HOME/miniconda/envs/build"
+    PREFIX="/usr/local/miniconda/envs/build"
 
     # Set up build flags for cibuildwheel
     # This is needed to bundle libomp lib we downloaded earlier

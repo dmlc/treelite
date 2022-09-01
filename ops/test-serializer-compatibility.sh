ce976a48b855334edc427bdcb4749de624151fb5
Hyunsu Philip Cho <chohyu01@cs.washington.edu>
2022-09-01 03:02:39 -0700

    Add round-trip test with current version

--- ops/test-serializer-compatibility.sh
+++ ops/test-serializer-compatibility.sh
@@ -17,9 +17,18 @@ cd ..
 
 CURRENT_VERSION=$(cat python/treelite/VERSION)
 
-echo "##[section]Testing backward compatibility of serialization: 2.4 -> ${CURRENT_VERSION}"
+set -x
+echo "##[section]Testing serialization: 2.4 -> ${CURRENT_VERSION}"
 python tests/cython/compatibility_tester.py --task save --checkpoint-path checkpoint.bin \
   --model-pickle-path model.pkl --expected-treelite-version 2.4.0
+PYTHONPATH=./python/:./runtime/python/ python tests/cython/compatibility_tester.py --task load \
+  --checkpoint-path checkpoint.bin --model-pickle-path model.pkl \
+  --expected-treelite-version ${CURRENT_VERSION}
+
+echo "##[section]Testing serialization: ${CURRENT_VERSION} -> ${CURRENT_VERSION}"
+PYTHONPATH=./python/:./runtime/python/ python tests/cython/compatibility_tester.py --task save \
+  --checkpoint-path checkpoint.bin --model-pickle-path model.pkl \
+  --expected-treelite-version ${CURRENT_VERSION}
 PYTHONPATH=./python/:./runtime/python/ python tests/cython/compatibility_tester.py --task load \
   --checkpoint-path checkpoint.bin --model-pickle-path model.pkl \
   --expected-treelite-version ${CURRENT_VERSION}

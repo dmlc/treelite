20281b070e6b1e22bf1ed091206ba4ff6db10ba1
Hyunsu Philip Cho <chohyu01@cs.washington.edu>
2022-08-31 18:07:14 -0700

    [CI] Use Conda everywhere

--- ops/test-linux-python-wheel.sh
+++ ops/test-linux-python-wheel.sh
@@ -3,10 +3,12 @@
 set -euo pipefail
 
 echo "##[section]Setting up Python environment..."
-python -m pip install --upgrade pip numpy scipy pandas pytest scikit-learn xgboost lightgbm awscli
+conda create -n dev -y -q -c conda-forge python=3.9 numpy scipy pandas pytest scikit-learn awscli
+conda activate dev
+pip install xgboost lightgbm
 
 echo "##[section]Installing Treelite into Python environment..."
-python -m pip install main/*.whl runtime/*.whl
+pip install main/*.whl runtime/*.whl
 
 echo "##[section]Uploading Python wheels..."
 python -m awscli s3 cp main/*.whl s3://treelite-wheels/ --acl public-read || true

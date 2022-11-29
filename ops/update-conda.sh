2a7e272d69369ed345b7409343cfe379feba450b
Hyunsu Cho <chohyu01@cs.washington.edu>
2022-11-28 20:52:02 -0800

    Debug

--- ops/update-conda.sh
+++ ops/update-conda.sh
@@ -4,5 +4,5 @@ set -euo pipefail
 
 echo "##[section]Update Conda"
 # Workaround for mamba-org/mamba#488
-#rm /usr/local/miniconda/pkgs/cache/*.json
+rm /usr/share/miniconda/pkgs/cache/*.json
 conda update -n base -c conda-forge -q -y conda

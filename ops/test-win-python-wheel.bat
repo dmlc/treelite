d0fafa4dc58c04f51d587d25ec64da21f7aef15b
Hyunsu Cho <chohyu01@cs.washington.edu>
2022-11-28 19:34:58 -0800

    Error hygiene for Windows pipeline

--- ops/test-win-python-wheel.bat
+++ ops/test-win-python-wheel.bat
@@ -1,18 +1,38 @@
 echo ##[section]Setting up Python environment...
 conda install -c conda-forge -q -y mamba
+if %errorlevel% neq 0 exit /b %errorlevel%
 mamba env create -q -f ops/conda_env/dev.yml
+if %errorlevel% neq 0 exit /b %errorlevel%
 call activate dev
 
 echo ##[section]Installing Treelite into Python environment...
-for /R %%i in (main\\*.whl) DO python -m pip install "%%i"
-for /R %%i in (runtime\\*.whl) DO python -m pip install "%%i"
+setlocal enabledelayedexpansion
+for /R %%i in (main\\*.whl) DO (
+  python tests\ci_build\rename_whl.py "%%i" %COMMIT_ID% win_amd64
+  if !errorlevel! neq 0 exit /b !errorlevel!
+)
+for /R %%i in (runtime\\*.whl) DO (
+  python tests\ci_build\rename_whl.py "%%i" %COMMIT_ID% win_amd64
+  if !errorlevel! neq 0 exit /b !errorlevel!
+)
+for /R %%i in (main\\*.whl) DO (
+  python -m pip install "%%i"
+  if !errorlevel! neq 0 exit /b !errorlevel!
+)
+for /R %%i in (runtime\\*.whl) DO (
+  python -m pip install "%%i"
+  if !errorlevel! neq 0 exit /b !errorlevel!
+)
 
 echo ##[section]Running Python tests...
 mkdir temp
 python -m pytest --basetemp="%WORKING_DIR%\temp" -v -rxXs --fulltrace tests\python\test_basic.py
+if %errorlevel% neq 0 exit /b %errorlevel%
 
 echo ##[section]Uploading Python wheels...
-for /R %%i in (main\\*.whl) DO python tests\ci_build\rename_whl.py "%%i" %COMMIT_ID% win_amd64
-for /R %%i in (runtime\\*.whl) DO python tests\ci_build\rename_whl.py "%%i" %COMMIT_ID% win_amd64
-for /R %%i in (main\\*.whl) DO python -m awscli s3 cp "%%i" s3://treelite-wheels/ --acl public-read || cd .
-for /R %%i in (runtime\\*.whl) DO python -m awscli s3 cp "%%i" s3://treelite-wheels/ --acl public-read || cd .
+for /R %%i in (main\\*.whl) DO (
+  python -m awscli s3 cp "%%i" s3://treelite-wheels/ --acl public-read || cd .
+)
+for /R %%i in (runtime\\*.whl) DO (
+  python -m awscli s3 cp "%%i" s3://treelite-wheels/ --acl public-read || cd .
+)

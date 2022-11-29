a9f58c2f2b9fab8c9cd5ec4f657a9efc35fbbe8b
Hyunsu Cho <chohyu01@cs.washington.edu>
2022-11-29 01:01:48 -0800

    Fix

--- ops/build-macos.sh
+++ ops/build-macos.sh
@@ -6,6 +6,5 @@ echo "##[section]Building MacOS Python wheels..."
 tests/ci_build/build_macos_python_wheels.sh ${CIBW_PLATFORM_ID} ${COMMIT_ID}
 
 echo "##[section]Uploading MacOS Python wheels to S3..."
-micromamba activate dev
 python -m awscli s3 cp wheelhouse/treelite-*.whl s3://treelite-wheels/ --acl public-read || true
 python -m awscli s3 cp wheelhouse/treelite_runtime-*.whl s3://treelite-wheels/ --acl public-read || true

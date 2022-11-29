45b9a88f666fecf8dafc3415e5b985dabb15d44a
Hyunsu Philip Cho <chohyu01@cs.washington.edu>
2022-11-29 14:18:59 -0800

    Remove unused script

--- ops/update-conda.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-set -euo pipefail
-
-echo "##[section]Update Conda"
-# Workaround for mamba-org/mamba#488
-rm /usr/share/miniconda/pkgs/cache/*.json
-conda update -n base -c conda-forge -q -y conda

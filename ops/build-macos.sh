6fe6bc036dcc0e7b626f7212489fffd33e3721c1
Hyunsu Cho <chohyu01@cs.washington.edu>
2022-11-29 00:05:34 -0800

    Add pipeline to build MacOS wheels

--- ops/build-macos.sh
+++ ops/build-macos.sh
@@ -3,17 +3,15 @@
 set -euo pipefail
 
 echo "##[section]Setting up Python environment..."
-conda create -n python3 -y -q -c conda-forge python=3.9
-source activate python3
+micromamba deactivate
+micromamba activate dev
 pip install cibuildwheel
-source deactivate
+micromamba deactivate
 
 echo "##[section]Building MacOS Python wheels..."
-tests/ci_build/build_python_wheels.sh ${CIBW_PLATFORM_ID} ${COMMIT_ID}
+tests/ci_build/build_macos_python_wheels.sh ${CIBW_PLATFORM_ID} ${COMMIT_ID}
 
 echo "##[section]Uploading MacOS Python wheels to S3..."
-source activate python3
-python --version
-python -m pip install awscli
+micromamba activate dev
 python -m awscli s3 cp wheelhouse/treelite-*.whl s3://treelite-wheels/ --acl public-read || true
 python -m awscli s3 cp wheelhouse/treelite_runtime-*.whl s3://treelite-wheels/ --acl public-read || true

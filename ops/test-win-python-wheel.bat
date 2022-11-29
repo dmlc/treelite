7e52f2953592b8bac7d724d766c874a3d6529a4b
Hyunsu Cho <chohyu01@cs.washington.edu>
2022-11-28 22:58:25 -0800

    Add Windows wheel pipeline

--- ops/test-win-python-wheel.bat
+++ ops/test-win-python-wheel.bat
@@ -1,10 +1,5 @@
 echo ##[section]Setting up Python environment...
-conda update -n base -c conda-forge -q -y conda
-conda install -c conda-forge -y mamba>=1.0.0
-if %errorlevel% neq 0 exit /b %errorlevel%
-mamba env create -q -f ops/conda_env/dev.yml
-if %errorlevel% neq 0 exit /b %errorlevel%
-call activate dev
+call micromamba activate dev
 
 echo ##[section]Installing Treelite into Python environment...
 setlocal enabledelayedexpansion

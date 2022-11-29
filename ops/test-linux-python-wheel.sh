81a0013f97bc3afd5889a1622d1c692773113b5e
Hyunsu Cho <chohyu01@cs.washington.edu>
2022-11-28 23:00:37 -0800

    Fix wheel path

--- ops/test-linux-python-wheel.sh
+++ ops/test-linux-python-wheel.sh
@@ -3,11 +3,11 @@
 set -euo pipefail
 
 echo "##[section]Installing Treelite into Python environment..."
-pip install main/*.whl runtime/*.whl
+pip install python/dist/*.whl runtime/python/dist/*.whl
 
 echo "##[section]Running Python tests..."
 python -m pytest -v -rxXs --fulltrace tests/python/test_basic.py
 
 echo "##[section]Uploading Python wheels..."
-python -m awscli s3 cp main/*.whl s3://treelite-wheels/ --acl public-read || true
-python -m awscli s3 cp runtime/*.whl s3://treelite-wheels/ --acl public-read || true
+python -m awscli s3 cp python/dist/*.whl s3://treelite-wheels/ --acl public-read || true
+python -m awscli s3 cp runtime/python/dist/*.whl s3://treelite-wheels/ --acl public-read || true
--- ops/test-win-python-wheel.bat
+++ ops/test-win-python-wheel.bat
@@ -3,19 +3,19 @@ call micromamba activate dev
 
 echo ##[section]Installing Treelite into Python environment...
 setlocal enabledelayedexpansion
-for /R %%i in (main\\*.whl) DO (
+for /R %%i in (python\\dist\\*.whl) DO (
   python tests\ci_build\rename_whl.py "%%i" %COMMIT_ID% win_amd64
   if !errorlevel! neq 0 exit /b !errorlevel!
 )
-for /R %%i in (runtime\\*.whl) DO (
+for /R %%i in (runtime\\python\\dist\\*.whl) DO (
   python tests\ci_build\rename_whl.py "%%i" %COMMIT_ID% win_amd64
   if !errorlevel! neq 0 exit /b !errorlevel!
 )
-for /R %%i in (main\\*.whl) DO (
+for /R %%i in (python\\dist\\*.whl) DO (
   python -m pip install "%%i"
   if !errorlevel! neq 0 exit /b !errorlevel!
 )
-for /R %%i in (runtime\\*.whl) DO (
+for /R %%i in (runtime\\python\\dist\\*.whl) DO (
   python -m pip install "%%i"
   if !errorlevel! neq 0 exit /b !errorlevel!
 )
@@ -26,9 +26,9 @@ python -m pytest --basetemp="%WORKING_DIR%\temp" -v -rxXs --fulltrace tests\pyth
 if %errorlevel% neq 0 exit /b %errorlevel%
 
 echo ##[section]Uploading Python wheels...
-for /R %%i in (main\\*.whl) DO (
+for /R %%i in (python\\dist\\*.whl) DO (
   python -m awscli s3 cp "%%i" s3://treelite-wheels/ --acl public-read || cd .
 )
-for /R %%i in (runtime\\*.whl) DO (
+for /R %%i in (runtime\\python\\dist\\*.whl) DO (
   python -m awscli s3 cp "%%i" s3://treelite-wheels/ --acl public-read || cd .
 )

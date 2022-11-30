46c53584dbc8aae57a1f7045e128906a5f782e9f
Hyunsu Philip Cho <chohyu01@cs.washington.edu>
2022-11-29 17:36:23 -0800

    [CI] Fix the script that names wheels

--- tests/ci_build/rename_whl.py
+++ tests/ci_build/rename_whl.py
@@ -1,35 +1,57 @@
-import sys
+import argparse
+import glob
 import os
+import sys
 from contextlib import contextmanager
+
 
 @contextmanager
 def cd(path):
     path = os.path.normpath(path)
     cwd = os.getcwd()
     os.chdir(path)
-    print("cd " + path)
+    print(f"cd {path}")
     try:
         yield path
     finally:
         os.chdir(cwd)
 
-if len(sys.argv) != 4:
-    print('Usage: {} [wheel to rename] [commit id] [platform tag]'.format(sys.argv[0]))
-    sys.exit(1)
 
-whl_path = sys.argv[1]
-commit_id = sys.argv[2]
-platform_tag = sys.argv[3]
+def main(args):
+    if not os.path.isdir(args.wheel_dir):
+        raise ValueError("wheel_dir argument must be a directory")
 
-dirname, basename = os.path.dirname(whl_path), os.path.basename(whl_path)
+    with cd(args.wheel_dir):
+        whl_list = list(glob.glob("*.whl"))
+        for whl_path in whl_list:
+            basename = os.path.basename(whl_path)
+            tokens = basename.split("-")
+            assert len(tokens) == 5
+            keywords = {
+                "pkg_name": tokens[0],
+                "version": tokens[1],
+                "commit_id": args.commit_id,
+                "platform_tag": args.platform_tag,
+            }
+            new_name = (
+                "{pkg_name}-{version}+{commit_id}-py3-none-{platform_tag}.whl".format(
+                    **keywords
+                )
+            )
+            print(f"Renaming {basename} to {new_name}...")
+            os.rename(basename, new_name)
 
-with cd(dirname):
-    tokens = basename.split('-')
-    assert len(tokens) == 5
-    keywords = {'pkg_name': tokens[0],
-                'version': tokens[1],
-                'commit_id': commit_id,
-                'platform_tag': platform_tag}
-    new_name = '{pkg_name}-{version}+{commit_id}-py3-none-{platform_tag}.whl'.format(**keywords)
-    print('Renaming {} to {}...'.format(basename, new_name))
-    os.rename(basename, new_name)
+
+if __name__ == "__main__":
+    description = (
+        "Script to rename wheel(s) using a commit ID and platform tag."
+        "Note: This script will not recurse into subdirectories."
+    )
+    parser = argparse.ArgumentParser(description=description)
+    parser.add_argument("wheel_dir", type=str, help="Directory containing wheels")
+    parser.add_argument("commit_id", type=str, help="Hash of current git commit")
+    parser.add_argument(
+        "platform_tag", type=str, help="Platform tag, PEP 425 compliant"
+    )
+    args = parser.parse_args()
+    main(args)

4be519648cb3d7a9b053ccc29b27a987ecee7e14
Hyunsu Philip Cho <chohyu01@cs.washington.edu>
2022-11-29 17:06:37 -0800

    [CI] Migrate to GitHub Actions

--- ops/build-macos.sh
+++ ops/build-macos.sh
@@ -2,18 +2,9 @@
 
 set -euo pipefail
 
-echo "##[section]Setting up Python environment..."
-conda create -n python3 -y -q -c conda-forge python=3.9
-source activate python3
-pip install cibuildwheel
-source deactivate
-
 echo "##[section]Building MacOS Python wheels..."
-tests/ci_build/build_python_wheels.sh ${CIBW_PLATFORM_ID} ${COMMIT_ID}
+tests/ci_build/build_macos_python_wheels.sh ${CIBW_PLATFORM_ID} ${COMMIT_ID}
 
 echo "##[section]Uploading MacOS Python wheels to S3..."
-source activate python3
-python --version
-python -m pip install awscli
 python -m awscli s3 cp wheelhouse/treelite-*.whl s3://treelite-wheels/ --acl public-read || true
 python -m awscli s3 cp wheelhouse/treelite_runtime-*.whl s3://treelite-wheels/ --acl public-read || true
--- ops/build-windows.bat
+++ ops/build-windows.bat
@@ -1,11 +1,20 @@
+echo ##[section]Generating Visual Studio solution...
+mkdir build
+cd build
+cmake .. -G"Visual Studio 16 2019" -A x64 -DBUILD_CPP_TEST=ON
+if %errorlevel% neq 0 exit /b %errorlevel%
+
+echo ##[section]Building Visual Studio solution...
+cmake --build . --config Release -- /m
+if %errorlevel% neq 0 exit /b %errorlevel%
+cd ..
+
 echo ##[section]Running C++ tests...
 .\build\treelite_cpp_test.exe
 if %errorlevel% neq 0 exit /b %errorlevel%
 
 echo ##[section]Setting up Python environment...
-conda create -n dev -y -q -c conda-forge python=3.9 numpy scipy scikit-learn pandas
-if %errorlevel% neq 0 exit /b %errorlevel%
-call activate dev
+call micromamba activate dev
 
 echo ##[section]Packaging Python wheel for Treelite...
 cd python

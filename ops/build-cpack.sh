146326956b90dfb0e7bc6f604de38973821f9099
Philip Hyunsu Cho <chohyu01@cs.washington.edu>
2023-05-08 21:12:48 -0700

    [CI] Make nightly builds of CPack for amd64 and aarch64 (#494)

---
+++ ops/build-cpack.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+set -euo pipefail
+
+echo "##[section] Building Treelite for amd64..."
+tests/ci_build/ci_build.sh ubuntu20 tests/ci_build/build_via_cmake.sh
+
+echo "##[section] Packing CPack for amd64..."
+tests/ci_build/ci_build.sh ubuntu20 bash -c "cd build/ && cpack -G TGZ"
+for tgz in build/treelite-*-Linux.tar.gz
+do
+  mv -v "${tgz}" "${tgz%-Linux.tar.gz}+${COMMIT_ID}-Linux-amd64.tar.gz"
+done
+
+echo "##[section]Uploading CPack for amd64..."
+python -m awscli s3 cp build/*.tar.gz s3://treelite-cpack/ --acl public-read || true
+
+rm -rf build/
+
+echo "##[section] Building Treelite for aarch64..."
+docker run --rm --privileged multiarch/qemu-user-static --reset -p yes
+tests/ci_build/ci_build.sh ubuntu20_aarch64 tests/ci_build/build_via_cmake.sh
+
+echo "##[section] Packing CPack for aarch64..."
+docker run --rm --privileged multiarch/qemu-user-static --reset -p yes
+tests/ci_build/ci_build.sh ubuntu20_aarch64 bash -c "cd build/ && cpack -G TGZ"
+for tgz in build/treelite-*-Linux.tar.gz
+do
+  mv -v "${tgz}" "${tgz%-Linux.tar.gz}+${COMMIT_ID}-Linux-aarch64.tar.gz"
+done
+
+echo "##[section]Uploading CPack for aarch64..."
+python -m awscli s3 cp build/*.tar.gz s3://treelite-cpack/ --acl public-read || true
--- ops/build-linux.sh
+++ ops/build-linux.sh
@@ -5,18 +5,18 @@ set -euo pipefail
 TAG=manylinux2014_x86_64
 
 echo "##[section]Building Treelite..."
-tests/ci_build/ci_build.sh cpu tests/ci_build/build_via_cmake.sh
+tests/ci_build/ci_build.sh centos7 tests/ci_build/build_via_cmake.sh
 
 echo "##[section]Packaging Python wheel for Treelite..."
-tests/ci_build/ci_build.sh cpu bash -c "cd python/ && python setup.py bdist_wheel --universal"
+tests/ci_build/ci_build.sh centos7 bash -c "cd python/ && python setup.py bdist_wheel --universal"
 tests/ci_build/ci_build.sh auditwheel_x86_64 auditwheel repair --only-plat --plat ${TAG} python/dist/*.whl
 rm -v python/dist/*.whl
 mv -v wheelhouse/*.whl python/dist/
-tests/ci_build/ci_build.sh cpu python tests/ci_build/rename_whl.py python/dist ${COMMIT_ID} ${TAG}
+tests/ci_build/ci_build.sh centos7 python tests/ci_build/rename_whl.py python/dist ${COMMIT_ID} ${TAG}
 
 echo "##[section]Packaging Python wheel for Treelite runtime..."
-tests/ci_build/ci_build.sh cpu bash -c "cd runtime/python/ && python setup.py bdist_wheel --universal"
+tests/ci_build/ci_build.sh centos7 bash -c "cd runtime/python/ && python setup.py bdist_wheel --universal"
 tests/ci_build/ci_build.sh auditwheel_x86_64 auditwheel repair --only-plat --plat ${TAG} runtime/python/dist/*.whl
 rm -v runtime/python/dist/*.whl
 mv -v wheelhouse/*.whl runtime/python/dist/
-tests/ci_build/ci_build.sh cpu python tests/ci_build/rename_whl.py runtime/python/dist ${COMMIT_ID} ${TAG}
+tests/ci_build/ci_build.sh centos7 python tests/ci_build/rename_whl.py runtime/python/dist ${COMMIT_ID} ${TAG}

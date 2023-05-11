2d3616642a364624678ace28ffcbccd250f1a0e4
Hyunsu Philip Cho <chohyu01@cs.washington.edu>
2023-05-10 17:55:27 -0700

    Remove unused code

--- python/treelite/util.py
+++ python/treelite/util.py
@@ -21,23 +21,9 @@ _NUMPY_TYPE_TABLE = {
     'float64': np.float64
 }
 
-_NUMPY_TYPE_TABLE_INV = {
-    np.uint32: 'unit32',
-    np.float32: 'float32',
-    np.float64: 'float64'
-}
-
 
 class TreeliteError(Exception):
     """Error thrown by Treelite"""
-
-
-def buffer_from_memory(ptr, size):
-    """Make Python buffer from raw memory"""
-    func = ctypes.pythonapi.PyMemoryView_FromMemory
-    func.restype = ctypes.py_object
-    PyBUF_READ = 0x100  # pylint: disable=C0103
-    return func(ptr, size, PyBUF_READ)
 
 
 def c_str(string):
@@ -62,16 +48,6 @@ def _warn_callback(msg: bytes) -> None:
     warnings.warn(py_str(msg))
 
 
-def lineno():
-    """Returns line number"""
-    return inspect.currentframe().f_back.f_lineno
-
-
-def log_info(filename, linenum, msg):
-    """Mimics behavior of the logging macro LOG(INFO) in dmlc-core"""
-    print(f'[{time.strftime("%X")}] {filename}:{linenum}: {msg}')
-
-
 def type_info_to_ctypes_type(type_info):
     """Obtain ctypes type corresponding to a given TypeInfo"""
     return _CTYPES_TYPE_TABLE[type_info]
@@ -80,8 +56,3 @@ def type_info_to_ctypes_type(type_info):
 def type_info_to_numpy_type(type_info):
     """Obtain ctypes type corresponding to a given TypeInfo"""
     return _NUMPY_TYPE_TABLE[type_info]
-
-
-def numpy_type_to_type_info(type_info):
-    """Obtain TypeInfo corresponding to a given NumPy type"""
-    return _NUMPY_TYPE_TABLE_INV[type_info]

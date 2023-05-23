11aa83941eabccaeb636582d3c6961bc0368dfef
Hyunsu Philip Cho <chohyu01@cs.washington.edu>
2023-05-23 14:45:08 -0700

    Add script to change version

--- cmake/Version.cmake
+++ cmake/Version.cmake
@@ -1,9 +1,6 @@
 function (write_version)
     message(STATUS "Treelite VERSION: ${PROJECT_VERSION}")
     configure_file(
-            ${PROJECT_SOURCE_DIR}/cmake/Python_version.in
-            ${PROJECT_SOURCE_DIR}/python/treelite/VERSION @ONLY)
-    configure_file(
             ${PROJECT_SOURCE_DIR}/cmake/version.h.in
             include/treelite/version.h)
 endfunction (write_version)

c5075e8488995a77687bd3d1a352cc6e209ae301
Hyunsu Philip Cho <chohyu01@cs.washington.edu>
2023-07-31 19:56:36 -0700

    Use std:: prefix for uint32_t and size_t

--- include/treelite/data.h
+++ include/treelite/data.h
@@ -9,6 +9,8 @@
 
 #include <treelite/typeinfo.h>
 
+#include <cstddef>
+#include <cstdint>
 #include <memory>
 #include <type_traits>
 #include <vector>
@@ -19,9 +21,9 @@ enum class DMatrixType : uint8_t { kDense = 0, kSparseCSR = 1 };
 
 class DMatrix {
  public:
-  virtual size_t GetNumRow() const = 0;
-  virtual size_t GetNumCol() const = 0;
-  virtual size_t GetNumElem() const = 0;
+  virtual std::size_t GetNumRow() const = 0;
+  virtual std::size_t GetNumCol() const = 0;
+  virtual std::size_t GetNumElem() const = 0;
   virtual DMatrixType GetType() const = 0;
   virtual TypeInfo GetElementType() const = 0;
   DMatrix() = default;
@@ -34,16 +36,16 @@ class DenseDMatrix : public DMatrix {
 
  public:
   template <typename ElementType>
-  static std::unique_ptr<DenseDMatrix> Create(
-      std::vector<ElementType> data, ElementType missing_value, size_t num_row, size_t num_col);
+  static std::unique_ptr<DenseDMatrix> Create(std::vector<ElementType> data,
+      ElementType missing_value, std::size_t num_row, std::size_t num_col);
   template <typename ElementType>
   static std::unique_ptr<DenseDMatrix> Create(
-      void const* data, void const* missing_value, size_t num_row, size_t num_col);
-  static std::unique_ptr<DenseDMatrix> Create(
-      TypeInfo type, void const* data, void const* missing_value, size_t num_row, size_t num_col);
-  size_t GetNumRow() const override = 0;
-  size_t GetNumCol() const override = 0;
-  size_t GetNumElem() const override = 0;
+      void const* data, void const* missing_value, std::size_t num_row, std::size_t num_col);
+  static std::unique_ptr<DenseDMatrix> Create(TypeInfo type, void const* data,
+      void const* missing_value, std::size_t num_row, std::size_t num_col);
+  std::size_t GetNumRow() const override = 0;
+  std::size_t GetNumCol() const override = 0;
+  std::size_t GetNumElem() const override = 0;
   DMatrixType GetType() const override = 0;
   TypeInfo GetElementType() const override;
 };
@@ -56,28 +58,28 @@ class DenseDMatrixImpl : public DenseDMatrix {
   /*! \brief value representing the missing value (usually NaN) */
   ElementType missing_value;
   /*! \brief number of rows */
-  size_t num_row;
+  std::size_t num_row;
   /*! \brief number of columns (i.e. # of features used) */
-  size_t num_col;
+  std::size_t num_col;
 
   DenseDMatrixImpl() = delete;
-  DenseDMatrixImpl(
-      std::vector<ElementType> data, ElementType missing_value, size_t num_row, size_t num_col);
+  DenseDMatrixImpl(std::vector<ElementType> data, ElementType missing_value, std::size_t num_row,
+      std::size_t num_col);
   ~DenseDMatrixImpl() = default;
   DenseDMatrixImpl(DenseDMatrixImpl const&) = default;
   DenseDMatrixImpl(DenseDMatrixImpl&&) noexcept = default;
   DenseDMatrixImpl& operator=(DenseDMatrixImpl const&) = default;
   DenseDMatrixImpl& operator=(DenseDMatrixImpl&&) noexcept = default;
 
-  size_t GetNumRow() const override;
-  size_t GetNumCol() const override;
-  size_t GetNumElem() const override;
+  std::size_t GetNumRow() const override;
+  std::size_t GetNumCol() const override;
+  std::size_t GetNumElem() const override;
   DMatrixType GetType() const override;
 
   template <typename OutputType>
-  void FillRow(size_t row_id, OutputType* out) const;
+  void FillRow(std::size_t row_id, OutputType* out) const;
   template <typename OutputType>
-  void ClearRow(size_t row_id, OutputType* out) const;
+  void ClearRow(std::size_t row_id, OutputType* out) const;
 
   friend class DenseDMatrix;
 };
@@ -89,15 +91,17 @@ class CSRDMatrix : public DMatrix {
  public:
   template <typename ElementType>
   static std::unique_ptr<CSRDMatrix> Create(std::vector<ElementType> data,
-      std::vector<uint32_t> col_ind, std::vector<size_t> row_ptr, size_t num_row, size_t num_col);
+      std::vector<std::uint32_t> col_ind, std::vector<std::size_t> row_ptr, std::size_t num_row,
+      std::size_t num_col);
   template <typename ElementType>
-  static std::unique_ptr<CSRDMatrix> Create(void const* data, uint32_t const* col_ind,
-      size_t const* row_ptr, size_t num_row, size_t num_col);
+  static std::unique_ptr<CSRDMatrix> Create(void const* data, std::uint32_t const* col_ind,
+      std::size_t const* row_ptr, std::size_t num_row, std::size_t num_col);
   static std::unique_ptr<CSRDMatrix> Create(TypeInfo type, void const* data,
-      uint32_t const* col_ind, size_t const* row_ptr, size_t num_row, size_t num_col);
-  size_t GetNumRow() const override = 0;
-  size_t GetNumCol() const override = 0;
-  size_t GetNumElem() const override = 0;
+      std::uint32_t const* col_ind, std::size_t const* row_ptr, std::size_t num_row,
+      std::size_t num_col);
+  std::size_t GetNumRow() const override = 0;
+  std::size_t GetNumCol() const override = 0;
+  std::size_t GetNumElem() const override = 0;
   DMatrixType GetType() const override = 0;
   TypeInfo GetElementType() const override;
 };
@@ -108,31 +112,31 @@ class CSRDMatrixImpl : public CSRDMatrix {
   /*! \brief feature values */
   std::vector<ElementType> data;
   /*! \brief feature indices. col_ind[i] indicates the feature index associated with data[i]. */
-  std::vector<uint32_t> col_ind;
+  std::vector<std::uint32_t> col_ind;
   /*! \brief pointer to row headers; length is [num_row] + 1. */
-  std::vector<size_t> row_ptr;
+  std::vector<std::size_t> row_ptr;
   /*! \brief number of rows */
-  size_t num_row;
+  std::size_t num_row;
   /*! \brief number of columns (i.e. # of features used) */
-  size_t num_col;
+  std::size_t num_col;
 
   CSRDMatrixImpl() = delete;
-  CSRDMatrixImpl(std::vector<ElementType> data, std::vector<uint32_t> col_ind,
-      std::vector<size_t> row_ptr, size_t num_row, size_t num_col);
+  CSRDMatrixImpl(std::vector<ElementType> data, std::vector<std::uint32_t> col_ind,
+      std::vector<std::size_t> row_ptr, std::size_t num_row, std::size_t num_col);
   CSRDMatrixImpl(CSRDMatrixImpl const&) = default;
   CSRDMatrixImpl(CSRDMatrixImpl&&) noexcept = default;
   CSRDMatrixImpl& operator=(CSRDMatrixImpl const&) = default;
   CSRDMatrixImpl& operator=(CSRDMatrixImpl&&) noexcept = default;
 
-  size_t GetNumRow() const override;
-  size_t GetNumCol() const override;
-  size_t GetNumElem() const override;
+  std::size_t GetNumRow() const override;
+  std::size_t GetNumCol() const override;
+  std::size_t GetNumElem() const override;
   DMatrixType GetType() const override;
 
   template <typename OutputType>
-  void FillRow(size_t row_id, OutputType* out) const;
+  void FillRow(std::size_t row_id, OutputType* out) const;
   template <typename OutputType>
-  void ClearRow(size_t row_id, OutputType* out) const;
+  void ClearRow(std::size_t row_id, OutputType* out) const;
 
   friend class CSRDMatrix;
 };

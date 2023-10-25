ffdc96bc6fa6ff2414f30e6b56b666f3371b298c
Hyunsu Cho <chohyu01@cs.washington.edu>
2023-10-24 21:33:57 -0700

    Update docstring

--- python/treelite/sklearn/__init__.py
+++ python/treelite/sklearn/__init__.py
@@ -4,7 +4,10 @@ from .importer import import_model
 
 
 def import_model_with_model_builder(sklearn_model):
-    """Stub for removed function"""
+    """
+    This function was removed in Treelite 4.0. Please use :py:meth:`treelite.sklearn.import_model`
+    instead.
+    """
     raise NotImplementedError(
         "treelite.sklearn.import_model_with_model_builder() was removed in Treelite 4.0. "
         "Please use treelite.sklearn.import_model() instead."

2e8113ec3c4a6b2cbf82c7353c0a7bd4cf83ad3a
Hyunsu Cho <chohyu01@cs.washington.edu>
2023-12-18 14:56:13 -0800

    [CI] Build Python wheel for aarch64

--- ops/build-linux-aarch64.sh
+++ ops/build-linux-aarch64.sh
@@ -2,14 +2,15 @@
 
 set -euo pipefail
 
-TAG=manylinux2014_x86_64
+TAG=manylinux2014_aarch64
 
-echo "##[section]Building Python wheel (amd64) for Treelite..."
+echo "##[section]Building Python wheel (aarch64) for Treelite..."
+docker run --rm --privileged multiarch/qemu-user-static --reset -p yes
 tests/ci_build/ci_build.sh centos7_amd64 bash -c "cd python/ && pip wheel --no-deps -v . --wheel-dir dist/"
 tests/ci_build/ci_build.sh centos7_amd64 auditwheel repair --only-plat --plat ${TAG} python/dist/*.whl
 rm -v python/dist/*.whl
 mv -v wheelhouse/*.whl python/dist/
 python tests/ci_build/rename_whl.py python/dist ${COMMIT_ID} ${TAG}
 
-echo "##[section]Uploading Python wheel (amd64)..."
+echo "##[section]Uploading Python wheel (aarch64)..."
 python -m awscli s3 cp python/dist/*.whl s3://treelite-wheels/ --acl public-read --region us-west-2
--- ops/test-linux-python-wheel.sh
+++ ops/test-linux-python-wheel.sh
@@ -7,6 +7,3 @@ pip install python/dist/*.whl
 
 echo "##[section]Running Python tests..."
 python -m pytest -v -rxXs --fulltrace --durations=0 tests/python/test_sklearn_integration.py
-
-echo "##[section]Uploading Python wheels..."
-python -m awscli s3 cp python/dist/*.whl s3://treelite-wheels/ --acl public-read --region us-west-2

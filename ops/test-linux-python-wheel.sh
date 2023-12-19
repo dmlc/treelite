d1d6219f6ef64ef54db609f7f61a34ba306eb065
Philip Hyunsu Cho <chohyu01@cs.washington.edu>
2023-12-18 23:32:23 -0800

    [CI] Build Python wheel for aarch64 (#533)

--- ops/test-linux-python-wheel.sh
+++ ops/test-linux-python-wheel.sh
@@ -3,10 +3,7 @@
 set -euo pipefail
 
 echo "##[section]Installing Treelite into Python environment..."
-pip install python/dist/*.whl
+pip install wheelhouse/*.whl
 
 echo "##[section]Running Python tests..."
 python -m pytest -v -rxXs --fulltrace --durations=0 tests/python/test_sklearn_integration.py
-
-echo "##[section]Uploading Python wheels..."
-python -m awscli s3 cp python/dist/*.whl s3://treelite-wheels/ --acl public-read --region us-west-2 || true

74e827a5c51a769c09986e9aff43c2bb3c55275d
Hyunsu Cho <chohyu01@cs.washington.edu>
2024-02-14 02:51:31 -0800

    Add unit test

--- tests/python/test_xgboost_integration.py
+++ tests/python/test_xgboost_integration.py
@@ -565,3 +565,7 @@ def test_xgb_multi_target_regressor(
         expected_pred = xgb_model.predict(xgb.DMatrix(X_pred), validate_features=False)
         expected_pred = np.transpose(expected_pred[:, :, np.newaxis], axes=(1, 0, 2))
         np.testing.assert_almost_equal(out_pred, expected_pred, decimal=3)
+
+def test_load_old_xgboost_model():
+    path = pathlib.Path(__file__).parent.parent / "examples" / "mushroom" / "mushroom.model"
+    tl_model = treelite.frontend.load_xgboost_model_legacy_binary(path)  # should not crash

fix: Depth Calculation for iForest importer (#617)
* fix: Depth Calculation for iForest importer

Corrected the path depth calculation for `IsolationForest` model conversion
to match sklearn's implementation.

In Isolation Forest, the anomaly score is computed as:

    s(x) = 2^(-E[h(x)] / c(n))

Where:
- E[h(x)] is the expected path length for sample x
- c(n) = 2 * H(n - 1) - (2 * (n - 1)) / n
- H(n) is the nth harmonic number

Previously, Treelite used the digamma-based formulation for the harmonic number:

    H(n) = ψ(n + 1) + γ

and computed c(n) as:

    c(n) = float(2 * (harmonic(n) - 1))

While mathematically valid, this differs from sklearn’s implementation
and the original paper. sklearn approximates H(n) as:

    H(n) ≈ ln(n) + γ

and computes:

    c(n) = 2 * H(n - 1) - (2 * (n - 1)) / n

This PR changes Treelite’s calculation to match sklearn's implementation,
ensuring consistency in path length and anomaly score computations,
especially for small n where numerical differences may affect results.

* Fix formatting

* Update pytest

---------

Co-authored-by: Philip Hyunsu Cho <chohyu01@cs.washington.edu>
Co-authored-by: Hyunsu Cho <phcho@nvidia.com>

tests/python/test_sklearn_integration.py
@@ -192,7 +192,7 @@ def test_skl_converter_iforest(dataset):
 
     tl_model = treelite.sklearn.import_model(clf)
     out_pred = treelite.gtil.predict(tl_model, X)
-    np.testing.assert_almost_equal(out_pred, expected_pred, decimal=2)
+    np.testing.assert_almost_equal(out_pred, expected_pred)
 
 
 @given(
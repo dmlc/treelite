5bf48255f12ec04946ab4d661a8a8b809e740ef9
Hyunsu Cho <phcho@nvidia.com>
2026-06-26 01:25:33 -0700

    Upgrade to manylinux_2_28 standard

--- ops/build-linux-aarch64.sh
+++ ops/build-linux-aarch64.sh
@@ -2,12 +2,12 @@
 
 set -euo pipefail
 
-TAG=manylinux2014_aarch64
+TAG=manylinux_2_28_aarch64
 
-export CIBW_BUILD=cp38-manylinux_aarch64
+export CIBW_BUILD=cp312-manylinux_aarch64
 export CIBW_ARCHS=aarch64
 export CIBW_BUILD_VERBOSITY=3
-export CIBW_MANYLINUX_AARCH64_IMAGE=manylinux2014
+export CIBW_MANYLINUX_AARCH64_IMAGE=manylinux_2_28
 
 echo "##[section]Building Python wheel (aarch64) for Treelite..."
 python -m cibuildwheel python --output-dir wheelhouse

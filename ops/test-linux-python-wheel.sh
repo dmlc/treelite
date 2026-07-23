9dbe482dc9bfd267f68124b1348b6b0dcbff54e9
Cavan Riley <127157889+CavRiley@users.noreply.github.com>
2026-07-23 09:55:20 -0500

    Ci cibuildwheel migration (#672)
    * [CI] Run C++ tests on Windows coverage job
    
    * [CI] Build wheels with the official cibuildwheel action
    
    * [CI] Skip musllinux wheels

--- ops/test-linux-python-wheel.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-set -euo pipefail
-
-echo "##[section]Installing Treelite into Python environment..."
-pip install --force-reinstall wheelhouse/*.whl
-
-echo "##[section]Running Python tests..."
-python -m pytest -v -rxXs --fulltrace --durations=0 tests/python/test_sklearn_integration.py

f91ecf07f5f987bc1b7835f7aeb4903bef7ef188
James Lamb <jaylamb20@gmail.com>
2026-09-02 21:18:37 -0500

    use GoogleTest 1.18.0

--- cmake/CPMFindGTest.cmake
+++ cmake/CPMFindGTest.cmake
@@ -25,16 +25,6 @@ function(_treelite_find_gtest gtest_version)
   # not found, use CPM to download sources and build it
   message(STATUS "GTest not found, fetching GTest via CPM.")
 
-  # For MSVC, ensure GTest uses the same C runtime (/MD vs. /MT) as Treelite.
-  #
-  # As of v1.14.0, googletest did literal string replacements on CMAKE_CXX_FLAGS,
-  # and doesn't respect CMAKE_MSVC_RUNTIME_LIBRARY.
-  #
-  # TODO(jameslamb): remove this once this project uses at least GTest v1.18.0
-  # That version has this fix: https://github.com/google/googletest/pull/4877
-  #
-  set(gtest_force_shared_crt ${FORCE_SHARED_CRT} CACHE BOOL "" FORCE)
-
   # populate local CPM cache with googletest source (if not already populated), build targets
   include(${treelite_SOURCE_DIR}/cmake/CPMSetup.cmake)
 
@@ -51,15 +41,6 @@ function(_treelite_find_gtest gtest_version)
       "CMAKE_POSITION_INDEPENDENT_CODE ON"
       "INSTALL_GTEST OFF"
   )
-
-  if(MSVC)
-    foreach(target gtest gmock)
-      # For MSVC, ensure GTest targets propagate the same MSVC C Runtime
-      # to anything linking against them.
-      set_target_properties(${target} PROPERTIES
-          MSVC_RUNTIME_LIBRARY "${CMAKE_MSVC_RUNTIME_LIBRARY}")
-    endforeach()
-  endif()
 endfunction()
 
-_treelite_find_gtest(1.14.0)
+_treelite_find_gtest(1.18.0)
